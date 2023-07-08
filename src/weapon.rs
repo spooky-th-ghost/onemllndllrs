3c361a352108326ef5b0caf99c9ad02a5a2d7775
spooky <spooky@pop-os.localdomain>
2023-07-08 16:57:48 -0400

    switching

--- src/weapon.rs
+++ src/weapon.rs
@@ -1,9 +1,40 @@
 use bevy::prelude::*;
 
 pub struct Gun {
+    muzzle: Muzzle,
     receiver: Receiver,
     clip: Clip,
-    spread: Spread,
+    trigger: Trigger,
+}
+
+impl Gun {
+    pub fn fire(&mut self) {
+        if self.clip.spend_ammo() {
+            //TODO: Create Shot
+            use FireType::*;
+            match self.receiver.fire_type {
+                Hitscan => {}
+                Projectile => {}
+                ProjectileSpread => {}
+            }
+        } else {
+            //TODO: Play the click sound
+        }
+    }
+}
+
+pub enum Shot {
+    Hitscan {
+        base_damage: u16,
+    },
+    SingleProjectile {
+        base_damage: u16,
+    },
+    MultiProjectile {
+        base_damage: u16,
+        count: u8,
+        spread: f32,
+    },
 }
 
 pub struct Receiver {
@@ -18,13 +49,68 @@ pub struct Clip {
     reload_speed: f32,
 }
 
-pub struct Spread {
+impl Clip {
+    fn get_max(&self) -> u8 {
+        self.max
+    }
+
+    fn get_current(&self) -> u8 {
+        self.current
+    }
+
+    fn spend_ammo(&mut self) -> bool {
+        if self.current > 0 {
+            self.current -= 1;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn reload(&mut self) {
+        self.current = self.max;
+    }
+}
+
+pub struct Muzzle {
     min_spread: f32,
     max_spread: f32,
+    current_spread: f32,
     bloom: f32,
+    max_range: f32,
+}
+
+impl Muzzle {
+    fn get_spread(&self) -> f32 {
+        self.current_spread
+    }
+
+    fn get_range(&self) -> f32 {
+        self.max_range
+    }
+
+    fn increase_spread(&mut self) {
+        let start = self.current_spread;
+        let end = self.max_spread;
+        let t = self.bloom;
+        self.current_spread = start * (1.0 - t) + (end * t);
+    }
+
+    fn reduce_spread(&mut self) {
+        let start = self.current_spread;
+        let end = self.min_spread;
+        let t = self.bloom;
+        self.current_spread = start * (1.0 - t) + (end * t);
+    }
+}
+
+pub enum Trigger {
+    Auto,
+    SemiAuto,
 }
 
 pub enum FireType {
     Hitscan,
     Projectile,
+    ProjectileSpread,
 }
