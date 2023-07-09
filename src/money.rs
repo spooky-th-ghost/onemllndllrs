ef82b50649b4a66a220324a5989b3bf747a44ae8
spooky <spooky@pop-os.localdomain>
2023-07-09 00:01:02 -0400

    implment typecast and operator overloads for the new money type

--- src/money.rs
+++ src/money.rs
@@ -1,29 +1,83 @@
+use std::ops::{Add, AddAssign, Sub, SubAssign};
+
 use bevy::prelude::*;
 
 #[derive(Resource)]
 pub struct Debts {
-    medical: f32,
-    rent: f32,
-    utilities: f32,
+    medical: Money,
+    rent: Money,
+    utilities: Money,
 }
 
 impl Debts {
-    pub fn total(&self) -> f32 {
+    pub fn total(&self) -> Money {
         self.medical + self.rent + self.utilities
+    }
+
+    pub fn medical(&self) -> Money {
+        self.medical
+    }
+
+    pub fn rent(&self) -> Money {
+        self.rent
+    }
+
+    pub fn utilities(&self) -> Money {
+        self.utilities
     }
 }
 
 #[derive(Resource)]
 pub struct Wallet {
-    funds: f32,
+    funds: Money,
 }
 
 impl Wallet {
-    pub fn debit(&mut self, amount: f32) {
+    pub fn funds(&self) -> Money {
+        self.funds
+    }
+
+    pub fn debit(&mut self, amount: Money) {
         self.funds -= amount;
     }
 
-    pub fn credit(&mut self, amount: f32) {
+    pub fn credit(&mut self, amount: Money) {
         self.funds += amount;
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+pub struct Money(f32);
+
+impl From<f32> for Money {
+    fn from(value: f32) -> Self {
+        Money((value * 100.0).round() / 100.0)
+    }
+}
+
+impl Add for Money {
+    type Output = Self;
+    fn add(self, rhs: Self) -> Self::Output {
+        Money(self.0 + rhs.0)
+    }
+}
+
+impl AddAssign for Money {
+    fn add_assign(&mut self, rhs: Self) {
+        *self = Money(self.0 + rhs.0);
+    }
+}
+
+impl Sub for Money {
+    type Output = Money;
+
+    fn sub(self, rhs: Self) -> Self::Output {
+        Money(self.0 - rhs.0)
+    }
+}
+
+impl SubAssign for Money {
+    fn sub_assign(&mut self, rhs: Self) {
+        *self = Money(self.0 - rhs.0)
     }
 }
